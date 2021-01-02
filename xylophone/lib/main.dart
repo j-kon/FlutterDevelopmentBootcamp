7a298729cb4975ee631694a0f8779830bcac9742
Jeremiah Jacob <jaykon4me@gmail.com>
2021-01-02 01:23:39 +0100

    completed

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -4,22 +4,39 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) => Expanded(
+        child: FlatButton(
+          onPressed: () {
+            playSound(soundNumber);
+          },
+          color: color,
+          child: null,
+        ),
+      );
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildKey(color: Colors.red, soundNumber: 1),
+                buildKey(color: Colors.orange, soundNumber: 2),
+                buildKey(color: Colors.yellow, soundNumber: 3),
+                buildKey(color: Colors.green, soundNumber: 4),
+                buildKey(color: Colors.teal, soundNumber: 5),
+                buildKey(color: Colors.blue, soundNumber: 6),
+                buildKey(color: Colors.purple, soundNumber: 7),
+              ],
             ),
           ),
         ),
-      ),
-    );
-  }
+      );
 }
