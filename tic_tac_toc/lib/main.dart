c3937a7802616a7eb4951da1eae2708cc692b9db
Jeremiah Jacob <jaykon4me@gmail.com>
2021-01-18 04:45:51 +0100

    add if statement of 'O' and 'X'

--- tic_tac_toc/lib/main.dart
+++ tic_tac_toc/lib/main.dart
@@ -18,6 +18,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool oTurn = true;
   List<String> displayO = [
     '',
     '',
@@ -39,7 +40,7 @@ class _HomePageState extends State<HomePage> {
               SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
           itemBuilder: (BuildContext context, int index) {
             return GestureDetector(
-              onTap: ()  {
+              onTap: () {
                 _tapped(index);
               },
               child: Container(
@@ -59,7 +60,81 @@ class _HomePageState extends State<HomePage> {
 
   void _tapped(int index) {
     setState(() {
-      displayO[index] = 'O';
+      if (oTurn) {
+        displayO[index] = 'O';
+      } else {
+        displayO[index] = 'X';
+      }
+      oTurn = !oTurn;
+      _checkWinner();
     });
+  }
+
+  void _checkWinner() {
+    // check 1st row
+    if (displayO[0] == displayO[1] &&
+        displayO[0] == displayO[2] &&
+        displayO[0] != '') {
+      _showWinDialog(displayO[0],);
+    }
+
+    // check 2nd row
+    if (displayO[3] == displayO[4] &&
+        displayO[3] == displayO[5] &&
+        displayO[3] != '') {
+      _showWinDialog(displayO[3],);
+    }
+
+    // check 3rd row
+    if (displayO[6] == displayO[7] &&
+        displayO[6] == displayO[8] &&
+        displayO[6] != '') {
+      _showWinDialog(displayO[6],);
+    }
+
+    // check 1st column
+    if (displayO[0] == displayO[3] &&
+        displayO[0] == displayO[6] &&
+        displayO[0] != '') {
+      _showWinDialog(displayO[0],);
+    }
+
+    // check 2nd column
+    if (displayO[1] == displayO[4] &&
+        displayO[1] == displayO[7] &&
+        displayO[1] != '') {
+      _showWinDialog(displayO[1],);
+    }
+
+    // check 3rd column
+    if (displayO[2] == displayO[5] &&
+        displayO[2] == displayO[8] &&
+        displayO[2] != '') {
+      _showWinDialog(displayO[2],);
+    }
+
+    // check diagonal
+    if (displayO[6] == displayO[4] &&
+        displayO[6] == displayO[2] &&
+        displayO[6] != '') {
+      _showWinDialog(displayO[6],);
+    }
+
+    // check diagonal
+    if (displayO[0] == displayO[4] &&
+        displayO[0] == displayO[8] &&
+        displayO[0] != '') {
+      _showWinDialog(displayO[0],);
+    }
+  }
+
+  void _showWinDialog(String winner) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('WINNER IS:' + winner ),
+          );
+        });
   }
 }
